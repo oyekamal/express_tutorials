5f83acd2b8c586d7a636fa4235d311bb421fc245
oyekamal <autocratickamal1@gmail.com>
2023-03-27 15:22:06 +0500

    middleware added

--- core/things.js
+++ core/things.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.send('GET route on things.');
+router.get('/', function (req, res, next) {
+    res.send({ "message": "hello" });
+    next();
 });
-router.post('/', function (req, res) {
+
+router.post('/', function (req, res, next) {
     res.send('POST route on things.');
+    next();
 });
-router.get('/:id', function (req, res) {
+router.get('/:id', function (req, res, next) {
     res.send('The id you specified is ' + req.params.id);
+    next();
 });
-router.get('/regex/:id([0-9]{5})', function (req, res) {
+router.get('/regex/:id([0-9]{5})', function (req, res, next) {
     res.send('id: ' + req.params.id);
+    next();
 });
-router.get('/:name/:id', function (req, res) {
+router.get('/:name/:id', function (req, res, next) {
     res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
+    next();
 });
 
 //export this router to use in our index.js
--- index.js
+++ index.js
@@ -3,23 +3,31 @@ var things = require('./core/things.js');
 var app = express();
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send("get api here");
 });
 
 
-app.get("/hello", function(req, res){
-    res.send({"messaga":"hellow"});
+app.get("/hello", function (req, res) {
+    res.send({ "messaga": "hellow" });
 });
 
-app.post("/hello", function(req, res){
-    res.send({"messaga":"post"});
+app.post("/hello", function (req, res) {
+    res.send({ "messaga": "post" });
 });
 
 
-app.all('/test', function(req, res){
+app.all('/test', function (req, res) {
     res.send("HTTP method doesn't have any effect on this route!");
- });
-
+});
+//Middleware function to log request protocol
+app.use(function (req, res, next) {
+    console.log("A request Things start " + Date.now());
+    next();
+});
 app.use('/things', things);
+app.use('/things', function (req, res, next) {
+    console.log("A request Things end -- " + Date.now());
+    next();
+});
 app.listen(3000)
